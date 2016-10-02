bbf3cef5010d15577bd1cc4b337d61cd58994ed4
YSavenkov <ysavenkov@absolute.com>
2016-10-02 15:14:13 -0700

    WIP Home page

--- js/init.js
+++ js/init.js
@@ -4,5 +4,16 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+
+      if (scroll >= 50) {
+        $(".navbar-fixed").addClass("small-header");
+      } else {
+        $(".navbar-fixed").removeClass("small-header");
+      }
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
