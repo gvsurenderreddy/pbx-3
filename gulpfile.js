301808f7dd2e28434ecb40eef3b147fc1ee52bd9
YSavenkov <ysavenkov@absolute.com>
2016-10-10 12:23:54 -0700

    WIP add pages

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const browserSync = require('browser-sync').create();
+const del = require('del');
+const env = require('gulp-util').env;
+const gulp = require('gulp');
+const handlebars = require('gulp-compile-handlebars');
+const rename = require('gulp-rename');
+const livereload = require('gulp-livereload');
+
+const config = {
+  src: './src',
+  dest: './',
+  watchers: [
+    {
+      match: ['./src/**/*.hbs'],
+      tasks: ['html']
+    }
+  ]
+};
+// livereload({ start: true });
+
+gulp.task('clean', () => del(`${config.dest}/*.html`));
+
+gulp.task('html', ['clean'], () => {
+  return gulp.src(`${config.src}/pages/*.hbs`)
+    .pipe(handlebars({}, {
+      ignorePartials: true,
+      batch: [`${config.src}/partials`]
+    }))
+    .pipe(rename({
+      extname: '.html'
+    }))
+    .pipe(gulp.dest(config.dest));
+      // .pipe(livereload());
+
+});
+
+
+gulp.task('serve', () => {
+  browserSync.init({
+    open: false,
+    notify: false,
+    files: [`${config.dest}/**/*`],
+    server: config.dest
+  });
+});
+
+gulp.task('watch', () => {
+  config.watchers.forEach(item => {
+    // livereload.listen();
+    gulp.watch(item.match, item.tasks);
+  });
+});
+
+gulp.task('default', ['html'], done => {
+  if (env.dev) {
+    gulp.start('serve');
+    gulp.start('watch');
+  }
+  done();
+});
--- js/init.js
+++ js/init.js
@@ -4,15 +4,42 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
-
-    $(window).scroll(function() {
+    // $(window).off();
+    $(window).on('scroll', function() {
       var scroll = $(window).scrollTop();
 
       if (scroll >= 50) {
-        $(".navbar-fixed").addClass("small-header");
+        $('.navbar-fixed').addClass("small-header");
       } else {
-        $(".navbar-fixed").removeClass("small-header");
+        $('.navbar-fixed').removeClass("small-header");
       }
+
+      if ($('.track-image').length > 0) {
+        var el = $('.track-image');
+        el.css({opacity: 0});
+        var wh = $(window).height();
+        var elView  = wh - (el.offset().top - scroll);
+
+        if (elView > 0) {
+          el.css({opacity: Math.min(1 / (wh) * elView, 1)});
+        }
+      }
+    });
+
+    //Highlight active menu item
+    $(function() {
+      $('.nav-wrapper a[href="' + location.pathname.split("/")[location.pathname.split("/").length-1] + '"]').parent().addClass('active');
+    });
+
+    // Google maps no zoom on scrolling
+    $('.maps').off();
+
+    $('.maps').on('click', function () {
+      $('.maps iframe').css("pointer-events", "auto");
+    });
+
+    $( ".maps" ).on('mouseleave', function() {
+      $('.maps iframe').css("pointer-events", "none");
     });
 
   }); // end of document ready
